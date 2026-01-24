fb912a7c515327e0b2c19a492284170f7d696c52
sxkiss <admin@sxkiss.com>
2026-01-24 08:04:49 +0800

    666

--- WechatAPI/qq/__init__.py
+++
@@ -1,9 +0,0 @@
-from .message import QQMessageMixin
-from .friend import QQFriendMixin
-from .chatroom import QQChatroomMixin
- 
-__all__ = [
-    "QQMessageMixin",
-    "QQFriendMixin",
-    "QQChatroomMixin"
-] 
--- WechatAPI/qq/friend.py
+++
@@ -1,8 +0,0 @@
-import aiohttp
-
-class QQFriendMixin:
-    async def get_friend_list(self) -> dict:
-        """获取QQ好友列表"""
-        async with aiohttp.ClientSession() as session:
-            response = await session.get('http://localhost:5700/get_friend_list')
-            return await response.json() 
